535130a319abe2eb01c4d71da1cb1371c8604604
Rodrigo Santos <79957513+figuerarj@users.noreply.github.com>
2023-06-06 16:53:49 -0300

    pequenas correções no html e css; e finalização JS

---
+++ scripts.js
@@ -0,0 +1,29 @@
+const key = "1562a8c142ef4944f71d2543961d6236";
+
+function placeDataOnHtml(data) {
+  document.querySelector(".city").innerHTML = `Temperatura em ${data.name}`;
+  document.querySelector(".temp").innerHTML =
+    Math.floor(data.main.temp) + "º C";
+  document.querySelector(".climate").innerHTML = data.weather[0].description;
+  /*weather[0].toUpperCase() + weather.substr(1);*/
+  const iconcode = data.weather[0].icon;
+  const urlicon = "http://openweathermap.org/img/wn/" + iconcode + ".png";
+  document.querySelector(".img-temp").src = urlicon;
+  document.querySelector(
+    ".info-hum"
+  ).innerHTML = `Umidade: ${data.main.humidity}%`;
+}
+
+async function getData(city) {
+  const data = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
+  ).then((answer) => answer.json());
+
+  placeDataOnHtml(data);
+}
+
+function getNameCity() {
+  const city = document.querySelector(".input-city").value;
+
+  getData(city);
+}
